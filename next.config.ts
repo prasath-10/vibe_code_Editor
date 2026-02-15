94368aeeb40c9deb05914731ee6ed83508209a9a
Prasath <prasaths1007@gmail.com>
2026-02-15 18:29:43 +0530

    Disable ESLint blocking in production build

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,40 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    remotePatterns:[
-      {
-        protocol:"https",
-        hostname:"*",
-        port:'',
-        pathname:"/**"
-      }
-    ]
+  eslint: {
+    ignoreDuringBuilds: true,
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+
   async headers() {
     return [
       {
-        // Apply to all routes
-        source: '/:path*',
+        source: "/:path*",
         headers: [
           {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
           },
           {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
           },
         ],
       },
     ];
   },
-  reactStrictMode:false
+
+  reactStrictMode: false,
 };
 
 export default nextConfig;
